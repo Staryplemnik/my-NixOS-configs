9b3d67af3b95c26979d804918c93394a03477ee0
Damian <damixlinux@gmail.com>
2026-09-16 21:03:01 +0200

    Delete ghelper.nix

--- ghelper.nix
+++
@@ -1,7 +0,0 @@
-# Managed by g-helper install script. Safe to delete (also remove the
-# matching '[ /etc/nixos/ghelper.nix ] ++' from configuration.nix).
-{ ... }:
-{
-  imports = [ /etc/nixos/ghelper/nixos/module.nix ];
-  services.ghelper.enable = true;
-}
